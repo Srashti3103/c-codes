fec5b45ec6ed3da8acaad40b260c5facede65c73
srashti patel <srashtipatel31@gmail.com>
2025-09-03 13:45:13 +0530

    remove

---
+++ twelth.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main ()
+{
+    int num = 121;
+    int rem , res = 0;
+    while (num != 0)
+    {
+        rem = num % 10;
+        res = res + rem;
+        num = num /10;
+    }
+    if(num= 121 )
+    printf("It is a pallindrom ");
+    
+}
