efe5441845a917d18f2a0e6f05938d4097a84c86
srashti patel <srashtipatel31@gmail.com>
2025-09-24 23:55:31 +0530

    array

--- lec12/first.c
+++ lec12/first.c
@@ -2,9 +2,8 @@
 int main()
 {
     int arr[5]={1,1,3,4,5};
-    printf("%d",arr[0]);
     for (int i=0;i<5;i++){
-        printf("%d\n",arr[1]);
+        printf("%d\n",arr[i]);
     }
-return 0;
+    return 0;
 }
