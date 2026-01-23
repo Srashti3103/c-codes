80a6ea62a6f5eb4a2a67193cbc43984f94f60c7d
Srashti3103 <srashtipatel31@gmail.com>
2026-01-23 11:01:31 +0530

    untitled1.c

---
+++ lec26/Untitled-1.c
@@ -0,0 +1,18 @@
+//find unique number using bitwise operator
+#include<stdio.h>
+int main()
+{
+    int n;
+    printf("Enter the size of array :");
+    scanf("%d",&n);
+    int arr[n];
+    printf("Enter the elements of array :");
+    for(int i=0; i<n; i++){
+        scanf("%d",&arr[i]);
+    }
+    int xor=0;
+    for (int i=0; i<n; i++ ){
+        
+    }
+    printf("unique element : %d",xor);
+}
